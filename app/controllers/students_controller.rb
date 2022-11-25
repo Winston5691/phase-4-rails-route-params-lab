310fdf491c7c125bcfa50b3f386fc29e966a104e
Winston Langat <winston.langat@student.moringaschool.com>
2022-11-25 19:18:30 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,13 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def first_student
+    student = Student.find_by(id: params[:id])
+    render json:student
+  end
+  # def show
+  #   student = Student.find_by(first_name: params[{ key1: "value1", key2: "value2" }])
+  #   render json:student
+  # end
 
 end
